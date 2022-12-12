d3da440df1921cb2d2d99432979fde8269372248
Goran Matic <goran@fwd.hr>
2022-12-12 19:40:55 +0000

    fix for Django 4+ (change ugettext* to gettext* imports)

--- cmsplugin_plain_text/cms_plugins.py
+++ cmsplugin_plain_text/cms_plugins.py
@@ -1,17 +1,17 @@
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
-from django.utils.translation import ugettext as _
+from django.utils.translation import gettext as _
 
 from .models import Plaintext
 
 
 class PlaintextPlugin(CMSPluginBase):
     model = Plaintext
-    name = _(u"Plaintext")
-    render_template = 'cms/plugins/plaintext.html'
+    name = _("Plaintext")
+    render_template = "cms/plugins/plaintext.html"
 
     def render(self, context, instance, placeholder):
-        context['body'] = instance.body
+        context["body"] = instance.body
         return context
 
 
--- cmsplugin_plain_text/models.py
+++ cmsplugin_plain_text/models.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 from cms.models import CMSPlugin
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
 class Plaintext(CMSPlugin):
-    body = models.TextField(_('Plaintext'))
+    body = models.TextField(_("Plaintext"))
 
     def __unicode__(self):
         return self.body
